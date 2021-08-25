70ce8c64e0f2da5d48c1bfea91844187db48c15a
DESKTOP-848094R\super <superjun02@naver.com>
2021-08-25 19:40:41 +0900

    Lesson01 Quiz03

---
+++ src/main/java/com/quiz/lesson01/Lesson01Quiz03Controller.java
@@ -0,0 +1,13 @@
+package com.quiz.lesson01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Lesson01Quiz03Controller {
+
+	@RequestMapping("/lesson01/quiz03")
+	public String ex02() {
+		return "lesson01/quiz03";
+	}
+}
